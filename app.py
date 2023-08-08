6fd043c58cfb818389284d6d1159d2fb7ae85cf6
Rodrigo Silva <rodrigodamasceno115@gmail.com>
2023-08-08 16:20:52 -0400

    get purchase_orders_by_id

--- app.py
+++ app.py
@@ -13,6 +13,28 @@ purchase_orders = [
                 'price': 20.99
             }
         ]
+    },
+    {
+        'id': 2,
+        'description': 'Pedido de compras',
+        'items': [
+            {
+                'id': 3,
+                'description': 'Item do pedido 2',
+                'price': 20.99
+            }
+        ]
+    },
+    {
+        'id': 3,
+        'description': 'Pedido de compras',
+        'items': [
+            {
+                'id': 3,
+                'description': 'Item do pedido 3',
+                'price': 20.99
+            }
+        ]
     }
 ]
 
@@ -25,6 +47,14 @@ def home():
 @app.route('/purchase_orders')
 def get_purchase_orders():
     return jsonify(purchase_orders)
+
+
+@app.route('/purchase_orders/<int:id>')
+def get_purchase_orders_by_id(id):
+    for po in purchase_orders:
+        if po['id'] == id:
+            return jsonify(po)
+    return jsonify({'message': 'Pedido {} não encontrado'.format(id)})
 
 
 app.run(port=5000)
