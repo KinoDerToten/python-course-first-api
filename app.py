672691511a0b6624ab3057dffc3c0ce5de2fdfe6
Rodrigo Silva <rodrigodamasceno115@gmail.com>
2023-08-10 15:07:24 -0400

    create purchase orders items

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -54,6 +54,39 @@ def get_purchase_orders_by_id(id):
     for po in purchase_orders:
         if po['id'] == id:
             return jsonify(po)
+    return jsonify({'message': 'Pedido {} não encontrado'.format(id)})
+
+
+@app.route('/purchase_orders', methods=['POST'])
+def create_purchase_order():
+    request_data = request.get_json()
+    purchase_order = {
+        'id': request_data['id'],
+        'description': request_data['description'],
+        'items': []
+    }
+    purchase_orders.append(purchase_order)
+    return jsonify(purchase_order)
+
+
+@app.route('/purchase_orders/<int:id>/items')
+def get_purchase_orders_items(id):
+    for po in purchase_orders:
+        return jsonify(po['items'])
+
+
+@app.route('/purchase_orders/<int:id>/items', methods=['POST'])
+def create_purchase_orders_items(id):
+    req_data = request.get_json()
+    for po in purchase_orders:
+        po['items'].append(
+            {
+                'id': req_data['id'],
+                'description': req_data['description'],
+                'price': req_data['price']
+            }
+        )
+        return jsonify(po)
     return jsonify({'message': 'Pedido {} não encontrado'.format(id)})
 
 
